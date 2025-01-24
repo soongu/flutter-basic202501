6a1cdfe3d466562ff487138bffb1c568c8ec8876
soongu <naong526@naver.com>
2025-01-24 15:39:02 +0900

    - chap 3-1 :  #2 다양한 스타일 적용

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,51 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); // 부모 클래스의 Key를 전달받아 위젯 트리에 전달.
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('기본 텍스트 연습'),
+          title: const Text('다양한 텍스트 스타일'),
         ),
-        body: const Center(
-          child: Text(
-            '포켓몬스터',
-            style: TextStyle(
-              fontSize: 24, // 글자 크기
-              color: Colors.red, // 글자 색상
-              fontWeight: FontWeight.bold, // 굵기 (보통)
-              fontStyle: FontStyle.italic, // 기울임 없음
-              decoration: TextDecoration.lineThrough, // 장식 없음
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Text(
+              '큰 텍스트',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
-          ),
+            Text(
+              '파란색과 기울임 텍스트',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.blue,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Text(
+              '밑줄이 있는 텍스트',
+              style: TextStyle(
+                fontSize: 18,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.red,
+                decorationThickness: 2,
+              ),
+            ),
+            Text(
+              '취소선 텍스트',
+              style: TextStyle(
+                fontSize: 16,
+                decoration: TextDecoration.lineThrough,
+                decorationColor: Colors.green,
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
-
 }
